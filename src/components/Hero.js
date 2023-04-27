9e8c57a276e3320c3d6cb1c1cc226e1fbdc1cd0a
unartcs <unartcs@gmail.com>
2023-04-27 21:01:17 +0200

    Edited hero, navbar
    Added some styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Hero.css";
+import Navbar from '../components/Navbar'
+
 function Hero() {
-  return <div className="hero-wrapper">
-    
-  </div>;
+  return (
+    <div className="hero-wrapper">
+      <Navbar/>
+      <div className="button-container">
+        {/* <img src="/imgs/mountain1.jpg" alt="Hero" draggable="false" /> */}
+        <h2>Learn More</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -5,7 +5,7 @@ import Home from './Home'
 function Pages() {
   return (
     <div>
-        <Navbar/>
+        {/* <Navbar/> */}
         <Home/>
     </div>
   )
